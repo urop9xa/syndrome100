e3f89104f04e7c46e5618cc1f6de7fcacb00b7ca
urop9xa <urop9xa@gmail.com>
2020-12-15 23:31:47 +0300

    line2 added

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -1,6 +1,12 @@
 package ru.netology.statistic;
 
 public class StatisticsService {
+    /**
+     * Calculate index of max income
+     *
+     * @param incomes - array of incomes
+     * @return - index of first max value
+     */
     public long findMax(long[] incomes) {
         long current_max_index = 0;
         long current_max = incomes[0];
